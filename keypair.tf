a2b7ed7b74ab0bb04f6bda63775419ecb1999565
An Phan <phanan1309@gmail.com>
2024-05-21 19:27:06 +0700

    create keypair with tf

---
+++ keypair.tf
@@ -0,0 +1,15 @@
+resource "tls_private_key" "keypair" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "keypair" {
+    key_name = "jenkins_ec2"
+    public_key = tls_private_key.keypair.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.keypair.key_name}.pem"
+  content = tls_private_key.keypair.private_key_pem
+}
+
